4fb3a088b4eb204cbb47909f102d98e2e7e298fd
wolven531 <anthony.williams.cs@gmail.com>
2022-02-07 00:21:44 -0700

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	export let name: string;
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+</main>
+
+<style>
+	main {
+		margin: 0 auto;
+		max-width: 240px;
+		padding: 1em;
+		text-align: center;
+	}
+
+	h1 {
+		color: #ff3e00;
+		font-size: 4em;
+		font-weight: 100;
+		text-transform: uppercase;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
