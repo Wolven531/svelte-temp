be996479a3bd752d3c76108848f494dc62b3e1dd
wolven531 <anthony.williams.cs@gmail.com>
2022-02-15 21:18:56 -0700

    Formatting

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	export let name: string;
+	export let name: string
 </script>
 
 <main>
@@ -7,18 +7,18 @@
 </main>
 
 <style>
-	main {
-		margin: 0 auto;
-		max-width: 240px;
-		padding: 1em;
-		text-align: center;
-	}
-
 	h1 {
 		color: #ff3e00;
 		font-size: 4em;
 		font-weight: 100;
 		text-transform: uppercase;
+	}
+
+	main {
+		margin: 0 auto;
+		max-width: 240px;
+		padding: 1em;
+		text-align: center;
 	}
 
 	@media (min-width: 640px) {
